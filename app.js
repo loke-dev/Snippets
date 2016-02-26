93317d4dd3d72cfb34416c313121c36276c13aae
Loke Carlsson <lc222ak@student.lnu.se>
2016-02-26 20:37:27 +0100

    Initial structure

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 "use strict";
 
-console.log("c'',)");
+var express = require("express");
+var exphbs = require("express-handlebars");
+var app = express();
+
+app.engine("hbs", exphbs({
+    defaultLayout: "default",
+    extname: "hbs"
+}));
+
+app.set("view engine", "hbs");
+
+app.get("/", function(req, res) {
+    res.render("home/index");
+});
+
+
+
+
+/**
+ * Error handling!
+**/
+app.use(function(error, req, res, next) {
+    res.render("error/500");
+});
+
+//KEEP LAST!!!
+app.get("*", function(req, res){
+    res.render("error/404");
+});
+
+app.listen(3015, function() {
+    console.log("Example app listening on port 3015!");
+});
