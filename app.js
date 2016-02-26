3416cba52c2e0eb0b9143b130610ad57a0ae3beb
Loke Carlsson <lc222ak@student.lnu.se>
2016-02-26 21:35:13 +0100

    Added mongoose

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 "use strict";
 
-var express = require("express");
-var exphbs = require("express-handlebars");
-var app = express();
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const mongoose = require("./config/mongoose.js");
+
+//Start the server
+mongoose();
 
 app.engine("hbs", exphbs({
     defaultLayout: "default",
@@ -13,8 +17,8 @@ app.set("view engine", "hbs");
 
 app.get("/", function(req, res) {
     res.render("home/index");
+    console.log("HEMMMA!!");
 });
-
 
 
 
---
+++ config/mongoose.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const CONNECTION_STRING = "mongodb://localhost/mylittledb";
+
+module.exports =  function() {
+    let db = mongoose.connect(CONNECTION_STRING);
+
+    db.connection.on("connected", function() {
+        console.log("Mongoose connection open.");
+    });
+
+    db.connection.on("error", function(err) {
+        console.error("Mongoose connection error: ", err);
+    });
+
+    db.connection.on("disconnected", function() {
+        console.log("Mongoose connection disconnected.");
+    });
+
+    // If the Node process ends, close the Mongoose connection.
+    process.on("SIGINT", function() {
+        db.connection.close(function() {
+            console.log("Mongoose connection disconnected through app termination.");
+            process.exit(0);
+        });
+    });
+
+    return db;
+};
