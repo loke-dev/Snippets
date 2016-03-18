24db373acf26d14109a20f1da6586b7e5e404d1c
Loke Carlsson <lc222ak@student.lnu.se>
2016-03-18 23:49:06 +0100

    testing nginx

---
+++ public/js/loadCSS.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let fileref = document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", "/css/stylesheet.css");
+document.getElementsByTagName("head")[0].appendChild(fileref);
